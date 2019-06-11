fef18b78fad6f888f50003da2ab824922ea61291
cdyall <cdyall1@gmail.com>
2019-06-10 22:03:18 -0400

    still need readme

--- bamazonManager.js
+++ bamazonManager.js
@@ -8,14 +8,14 @@ var connection = mysql.createConnection({
   user: "root",
   password: "password",
   database: "bamazon_db"
-}); 
+});
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
 });
 
-var productTable = function() {
+var inventoryList = function() {
   var inquiry = "Select * FROM  products";
   connection.query(inquiry, function(err, res) {
     if (err) throw err;
@@ -24,7 +24,7 @@ var productTable = function() {
       colWidths: [9, 15, 20, 10, 10]
     });
     for (var i = 0; i < res.length; i++) {
-     displayTable.push([
+      displayTable.push([
         res[i].item_id,
         res[i].product_name,
         res[i].department_name,
@@ -33,6 +33,150 @@ var productTable = function() {
       ]);
     }
     console.log(displayTable.toString());
-    whichItem();
+    productUpdates();
+  });
+};
+
+function productUpdates() {
+  inquirer
+    .prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "what would you like to do with the current products?",
+        choices: ["Restock", "Input new product", "Delete a product"]
+      }
+    ])
+    .then(function(answers) {
+      switch (answers.action) {
+        case "Restock":
+          restockItems();
+          break;
+        case "Input new product":
+          newProduct();
+          break;
+        case "Delete a product":
+          trashProduct();
+          break;
+      }
+    });
+}
+
+function restockItems() {
+  inquirer
+    .prompt([
+      {
+        name: "ID",
+        type: "input",
+        message:
+          "pPlease input the number of the product you would like to restock."
+      },
+      {
+        name: "Amt",
+        type: "input",
+        message: "How many would you like to add?"
+      }
+    ])
+    .then(function(answers) {
+      var amount = answers.Amt;
+      var prodID = answers.ID;
+      itemRestock(prodID, amount);
+    });
+}
+
+function itemRestock(id, quant) {
+  connection.query("SELECT * FROM Products WHERE item_id = " + id, function(
+    err,
+    res
+  ) {
+    if (err) {
+      console.log(err);
+    }
+    connection.query(
+      "UPDATE Products SET stock_quantity = stock_quantity + " +
+        stock_quantity +
+        "WHERE item_id =" +
+        item_id
+    );
+
+    inventoryList();
   });
 }
+
+function newProduct() {
+  inquirer
+    .prompt([
+      {
+        name: "ID",
+        type: "input",
+        message: "ID Number"
+      },
+      {
+        name: "Name",
+        type: "input",
+        message: "What product would you like to add?"
+      },
+      {
+        name: "Category",
+        type: "input",
+        message: "What type of category would the product fit in?"
+      },
+      {
+        name: "Price",
+        type: "input",
+        message: "How much would it cost?"
+      },
+      {
+        name: "Quantity",
+        type: "input",
+        message: "How many would you like in inventory?"
+      }
+    ])
+    .then(function(answers) {
+      var id = answers.Id;
+      var name = answers.Name;
+      var category = answers.Category;
+      var price = answers.Price;
+      var quantity = answers.Quantity;
+      createProduct(id, name, category, price, quantity);
+    });
+}
+
+function createProduct(name, category, price, quantity) {
+  connection.query(
+    'INSERT INTO products (item_id,product_name,department_name,price,stock_quantity) VALUES("' +
+      id +
+      '","' +
+      name +
+      '","' +
+      category +
+      '",' +
+      price +
+      "," +
+      quantity +
+      ")"
+  );
+  inventoryList();
+}
+
+function trashProduct() {
+  inquirer
+    .prompt([
+      {
+        name: "ID",
+        type: "input",
+        message: "Which product do you wan to trash?"
+      }
+    ])
+    .then(function(answer) {
+      var id = answers.ID;
+      vanquishproduct(id);
+    });
+}
+
+function vanquishproduct(id) {
+  connection.query("DELETE FROM Products WHERE item_id = " + id);
+  inventoryList();
+}
+
+inventoryList();
